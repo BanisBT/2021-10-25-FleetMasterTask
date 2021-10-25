ecb3af1cbd73701a3def4a7e84eaca96c60bfab2
BanisBT <banisbt@gmail.com>
2021-10-25 11:50:18 +0300

    First commit

---
+++ src/main/java/com/tbarauskas/fleetmastertask/FleetMasterTaskApplication.java
@@ -0,0 +1,13 @@
+package com.tbarauskas.fleetmastertask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetMasterTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FleetMasterTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tbarauskas/fleetmastertask/FleetMasterTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tbarauskas.fleetmastertask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetMasterTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
